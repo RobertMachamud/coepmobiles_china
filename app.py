ff973920676ea116e7e841e99f07960b32f86503
Robert Machamud <robert_machamud@yahoo.com>
2022-03-28 05:29:59 +0000

    added index.html and produkt.html

--- app.py
+++ app.py
@@ -12,20 +12,26 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.secret_key = os.environ.get("SECRET_KEY")
+# app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+# app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+# app.secret_key = os.environ.get("SECRET_KEY")
+
+app.config["MONGO_DBNAME"] = "coep_cars"
+app.config["MONGO_URI"] = "mongodb+srv://robertTheUser:r00tUser@myfirstcluster.3kydg.mongodb.net/coep_cars?retryWrites=true&w=majority"
+app.secret_key = "rqP+bGT)p[kK<OBi5[Ph?6G(fqHte?"
 
 mongo = PyMongo(app)
 
 
 @app.route("/")
-def produkte():
-    # produkte = list(mongo.db.produkte.find())
-    # categories = list(mongo.db.categories.find())
-    # return render_template("index.html", produkte=produkte,
-    #                        categories=categories)
-    return render_template("index.html")
+def index():
+    produkte = list(mongo.db.cars.find())
+    return render_template("index.html", produkte=produkte)
+
+
+@app.route("/produkt")
+def produkt():
+    return render_template("produkt.html")
 
 
 if __name__ == "__main__":
